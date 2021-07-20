116a3959ef5a8cdabbd19c85f207a70262a36dbc
chenrenjie <707819156@qq.com>
2021-07-20 15:08:14 +0800

    none

---
+++ src/JUC/testNewBranch.java
@@ -0,0 +1,11 @@
+package JUC;
+
+/**
+ * Created by Chen
+ * 2021/7/20 14:50
+ */
+public class testNewBranch {
+    public static void main(String[] args) {
+        System.out.println("new branch");
+    }
+}
